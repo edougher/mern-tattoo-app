7e3643f07acb41e5b69221d035dcf5c28a01ec74
Aaron Dougher <aaron@Aarons-MacBook-Pro.local>
2021-06-15 00:41:45 -0500

    first commit

---
+++ client/src/components/RequestForm/RequestForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RequestForm = (props) => {
+    return (
+        <div>
+            <h1>Request Form</h1>
+        </div>
+    );
+}
+
+export default RequestForm;
