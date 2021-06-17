0d754c861837f29915ff819ae695dd79ce856a7c
Aaron Dougher <aaron@Aarons-MacBook-Pro.local>
2021-06-16 21:41:00 -0500

    setup App.js with browserRouter. Created Home.js, Auth.js, and Navbar.js components

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+
+import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core'
+import useStyles from './styles';
+
 
 const Navbar = (props) => {
+    const classes = useStyles()
     return (
-        <div>
-            
-        </div>
+        <AppBar className={classes.appBar} position="static" color="inherit">
+            <div className={classes.brandContainer}>
+                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Dark Rose</Typography>
+            </div>
+        </AppBar>
     );
 }
 
