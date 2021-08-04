a8649797362057e90f9f259413a1a5f2c0269b73
Aaron Dougher <ejdougher@gmail.com>
2021-08-03 23:03:17 -0500

    work on ImgageModal

--- client/src/components/RequestForm/ImgModal/ImgModal.js
+++ client/src/components/RequestForm/ImgModal/ImgModal.js
@@ -1,15 +1,18 @@
 import React from "react";
-import Modal from 'react-modal'
+import Modal from "react-modal";
 
 const ImageModal = ({ open, imgs, handleClose }) => {
+  console.log(imgs);
 
   return (
-    <div>
-      <Modal isOpen={open}>
-        <button onClick={handleClose}>Close</button>
-        
+    <Modal isOpen={open} onClick={handleClose}>
+      <button>Close</button>
+      {imgs.map((img) => {
+        return (
+          <img src={img} alt="reference material" height="500" width="400" style={{ "vertical-align":"middle"}}/>
+        );
+      })}
     </Modal>
-    </div>
   );
 };
 
